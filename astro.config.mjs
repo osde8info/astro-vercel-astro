71319032ef24007152cccf53888144577134cc34
Clive D <cdarra@github.com>
2022-12-30 19:39:37 +0000

    removed analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import libAnalytics from "@astrolib/analytics";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), libAnalytics()]
 });

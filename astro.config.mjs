2ab42661c2a34e19af305dedf95884a6c3022c07
Clive D <cdarra@github.com>
2022-12-30 19:14:23 +0000

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import libAnalytics from "@astrolib/analytics";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), libAnalytics()]
+});
